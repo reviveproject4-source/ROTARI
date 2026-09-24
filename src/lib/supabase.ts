bdb3d5284d599f4f85848b6cc6f9b26a6bbe694e
KabarSantri <reviveproject4@gmail.com>
2026-09-24 07:14:49 +0700

    fix(config): update target Supabase URL to oluzsthlxjxxpukxkcry

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "https://uzrqolqdqsispedbmtrl.supabase.co";
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV6cnFvbHFkcXNpc3BlZGJtdHJsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk5NjU0MzQsImV4cCI6MjA5NTU0MTQzNH0.placeholder";
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "https://oluzsthlxjxxpukxkcry.supabase.co";
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9sdXpzdGhseGp4eHB1a3hrY3J5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk5NjU0MzQsImV4cCI6MjA5NTU0MTQzNH0.dummy_anon_key_for_build";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
